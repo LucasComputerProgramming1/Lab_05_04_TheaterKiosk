d2b9a3686783f5e0084885a072c72b14abdc358e
Lucas <gibsonlc@mail.uc.edu>
2023-02-11 17:18:55 -0500

    Initial commit

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,13 @@
+public class TheaterKiosk {
+    public static void main(String[] args) {
+        int age = 0;
+        System.out.println("Please enter your age");
+        age = 21;
+        System.out.println("Testing for "+age);
+        if(age >= 21)
+        {
+            System.out.println("You get a wrist band");
+        }
+        else;
+    }
+}
