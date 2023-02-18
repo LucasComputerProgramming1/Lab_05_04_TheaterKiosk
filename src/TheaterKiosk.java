aa8f088419276352d8df6991f37b23fc70a4ceac
Lucas <gibsonlc@mail.uc.edu>
2023-02-18 11:11:44 -0500

    Project init

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,13 +1,18 @@
 public class TheaterKiosk {
     public static void main(String[] args) {
+        //Declare Variables
         int age = 0;
+        //Prompt
         System.out.println("Please enter your age");
+        //Input
         age = 21;
         System.out.println("Testing for "+age);
+        //Output If Over 21
         if(age >= 21)
         {
             System.out.println("You get a wrist band");
         }
+        //Output If Under 21 (End Program)
         else;
     }
 }
